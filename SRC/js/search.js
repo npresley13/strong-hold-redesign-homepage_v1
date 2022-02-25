2ed4d12a743624910ea1c1e3d432819142004249
Nathan Presley <npresley@strong-hold.com>
2022-02-25 14:02:37 -0500

    got a working search bar

--- SRC/js/search.js
+++ SRC/js/search.js
@@ -1,1 +1,34 @@
-const searchBox = document.querySelector('.nav-search');
+const products = [];
+const searchBox = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+fetch('src/js/data.json')
+    .then(response => response.json())
+    .then(data => products.push(...data));
+
+//console.log(products);
+
+// Matches input to sku field
+function findMatches(wordToMatch, products) {
+    return products.filter(product => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return product["Base SKU"].match(regex);
+    });
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, products);
+    const html = matchArray.map(product => {
+        const regex = new RegExp(this.value, 'gi');
+        const productName = product["Base SKU"].replace(regex, `<span class="h1">${this.value}</span>`);
+        return `
+            <li>
+                <span class="name">${productName}</span>
+            </li>
+        `
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+searchBox.addEventListener('change', displayMatches);
+searchBox.addEventListener('keyup', displayMatches);
